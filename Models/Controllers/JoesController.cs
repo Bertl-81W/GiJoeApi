8a7cc82a4ce7ac159754a9383210840e90994b0a
Bertl-81W <78581189+Bertl-81W@users.noreply.github.com>
2026-01-14 10:22:08 -0700

    add full CRUD support (Get, Post, Put, Delete)

--- Models/Controllers/JoesController.cs
+++ Models/Controllers/JoesController.cs
@@ -16,9 +16,14 @@ public class JoesController : ControllerBase
     }
 
     [HttpPost]
-    public void AddJoe(Joe newJoe)
+        public IActionResult AddJoe(Joe newJoe)
     {
         joes.Add(newJoe);
+        return CreatedAtAction(
+            nameof (GetJoeByName),
+            new { name = newJoe.Name },
+            newJoe
+            );    
     }
     [HttpGet("{name}")]
    public ActionResult<Joe> GetJoeByName(string name)
@@ -46,5 +51,22 @@ public IActionResult DeleteJoe(string name)
 
     joes.Remove(joe);
  return Ok($"Joe '{name}' has been removed.");
+}
+
+[HttpPut("{name}")]
+    public IActionResult UpdateJoe(string name, Joe updatedJoe)
+{
+    var joe = joes.FirstOrDefault(j =>
+        j.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+
+    if (joe == null)
+    {
+        return NotFound($"Joe '{name}' not found.");
+    }
+
+    joe.Name = updatedJoe.Name;
+    joe.Specialty = updatedJoe.Specialty;
+
+    return Ok(joe);
 }
 }
