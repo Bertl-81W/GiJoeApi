eef9f950222b256957f393f050581de2ae4368d5
Bertl-81W <78581189+Bertl-81W@users.noreply.github.com>
2026-01-22 09:11:39 -0700

    add GiJoeService to fetch external data

---
+++ Models/Services/GiJoeServices.cs
@@ -0,0 +1,29 @@
+using System.Net.Http;
+using System.Text.Json;
+using GiJoeApi.Models;
+
+namespace GiJoeApi.Services;
+
+public class GiJoeService
+{
+    private readonly HttpClient _httpClient;
+
+    public GiJoeService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<List<Joe>> GetExternalJoesAsync()
+    {
+        var response = await _httpClient.GetAsync("https://gijoe-api.onrender.com/api/characters");
+
+
+        if (!response.IsSuccessStatusCode)
+            return new List<Joe>();
+
+        var json = await response.Content.ReadAsStringAsync();
+
+        return JsonSerializer.Deserialize<List<Joe>>(json)
+               ?? new List<Joe>();
+    }
+}
