acc4e56bbb24c2faed69b9722a0e34671c3dd796
Bertl-81W <78581189+Bertl-81W@users.noreply.github.com>
2026-04-15 13:49:07 -0600

    configure database connection in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
-using GiJoeApi.Services;
+using GiJoeApi.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlite("Data Source=gijoe.db"));
 
 var app = builder.Build();
 
