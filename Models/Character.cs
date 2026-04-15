bb7a77a2fa1afb9477318aedae579db7d10b5565
Bertl-81W <78581189+Bertl-81W@users.noreply.github.com>
2026-04-15 13:41:08 -0600

    rename Joe model to Character and add Id for database

--- Models/Character.cs
+++ Models/Character.cs
@@ -1,7 +1,9 @@
 namespace GiJoeApi.Models;
 
-public class Joe
+public class Character
 {
+    public int Id { get; set; }
+    
     public string Name { get; set; } = string.Empty;
     public string PlaceOfBirth { get; set; } = string.Empty;    
     public string Specialty { get; set; } = string.Empty;
