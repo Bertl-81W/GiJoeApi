c688d530da03cd31147d21d9c27f88cff22999da
Bertl-81W <78581189+Bertl-81W@users.noreply.github.com>
2026-08-04 00:34:04 -0600

    feat: update character model for collector fields

--- Models/Character.cs
+++ Models/Character.cs
@@ -8,5 +8,6 @@ public class Character
     public string Accessories { get; set; } = string.Empty;    
     public string Specialty { get; set; } = string.Empty;    
     public string Faction { get; set; } = string.Empty;      
+    
     public bool HasFileCard { get; set; }
 }
